e77d201624825f8fbaa931af3c3cbdb12e4c4f20
kroki505 <mishob.2002@gmail.com>
2024-05-11 13:44:54 +0300

    initial project setup

---
+++ src/main/java/com/mido/MidoApplication.java
@@ -0,0 +1,13 @@
+package com.mido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidoApplication.class, args);
+	}
+
+}
