e8239302cb1609239497af9e0d0a94d8f7b008f0
Dian Vasilev <92683949+didov02@users.noreply.github.com>
2024-06-23 12:06:41 +0300

    Rating done

--- src/main/java/com/mido/services/UserRatingService.java
+++ src/main/java/com/mido/services/UserRatingService.java
@@ -1,29 +1,86 @@
 package com.mido.services;
 
+import com.mido.dtos.UserRatingDto;
+import com.mido.dtos.requests.UserRatingRequest;
+import com.mido.mappers.UserRatingMapper;
+import com.mido.models.User;
 import com.mido.models.UserRating;
 import com.mido.repositories.UserRatingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserRatingService {
 
-    private final UserRatingRepository userRatingRepository;
+    private final UserRatingRepository userRatingRepo;
 
-    public UserRating addUserRating(UserRating rating) {
-        userRatingRepository.save(rating);
+    private final UserRatingMapper ratingMapper;
+
+    private final UserService userService;
+
+    public void addUserRating(UserRatingRequest ratingReq) {
+        UserRating rating = setRatingFromRequest(ratingReq);
+        userRatingRepo.saveAndFlush(rating);
+    }
+
+    public UserRatingDto editUserRating(Long id, UserRatingRequest ratingReq) {
+        UserRating rating = setRatingFromRequest(ratingReq);
+        Optional<UserRating> optionalRating = userRatingRepo.findById(id);
+        UserRating editedRating = getEditedRating(optionalRating, rating);
+        userRatingRepo.saveAndFlush(editedRating);
+        return ratingMapper.ratingToRatingDto(editedRating);
+    }
+
+    private UserRating setRatingFromRequest(UserRatingRequest ratingReq) {
+        UserRating rating = new UserRating();
+
+        if(ratingReq.rating() < 1 || ratingReq.rating() > 5) {
+            throw new IllegalArgumentException("Rating is not in the accurate interval");
+        }
+
+        rating.setRating(ratingReq.rating());
+
+        if(ratingReq.userRates() == null || ratingReq.userRated() == null) {
+            throw new IllegalArgumentException("Usernames are empty");
+        }
+
+        if(userService.loadUserByUsername(ratingReq.userRates()) == null ||
+                userService.loadUserByUsername(ratingReq.userRated()) == null) {
+            throw new IllegalArgumentException("Such user doesn't exist");
+        }
+
+        rating.setUserRates(userService.loadUserByUsername(ratingReq.userRates()));
+        rating.setUserRated(userService.loadUserByUsername(ratingReq.userRated()));
+
         return rating;
     }
 
-    public UserRating editUserRating(UserRating rating) {
-        userRatingRepository.save(rating);
-        return rating;
+    private UserRating getEditedRating(Optional<UserRating> optionalRating, UserRating rating) {
+        if(optionalRating.isPresent()) {
+            UserRating newRating = optionalRating.get();
+
+            if(rating.getRating() != null) {
+                newRating.setRating(rating.getRating());
+            }
+            if(rating.getUserRates() != null) {
+                newRating.setUserRates(rating.getUserRates());
+            }
+            if(rating.getUserRated() != null) {
+                newRating.setUserRated(rating.getUserRated());
+            }
+
+            return newRating;
+        } else {
+            throw new IllegalArgumentException("Rating not found.");
+        }
     }
 
-    //removeUserRating
-    public void removeUserRating(Integer id) {
-        //userRatingRepository.delete(id);
+
+    public void removeUserRating(Long id) {
+        userRatingRepo.deleteById(id);
     }
 }
